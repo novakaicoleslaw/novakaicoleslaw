0ac41897a4245551994f2e7a6bd46a446925c445
Novakai Coleslaw <novakaicoleslaw.@gmail.com>
2025-12-11 11:10:24 -0500

    Updated homepage image and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
+    <main className="min-h-screen flex flex-col items-center justify-center bg-white text-black">
       <img 
-        src="/coleslaw.png" 
+        src="/CLLYLO.png" 
         alt="Novakai Coleslaw"
         className="w-full max-w-md mb-6"
       />
-      <p className="text-xl tracking-widest uppercase text-gray-300">
-        Website Under Construction
-      </p>
     </main>
   );
 }
